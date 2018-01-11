cdaa7a3c0cc5f246771d6880f4e09f224bfbb29d
Saba Saba <s2saba@uwaterloo.ca>
2018-01-11 13:28:54 -0500

    Landing page with sample songs.

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,5 +1,59 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import List, {ListItem, ListItemText, ListItemIcon} from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import MusicNoteIcon from 'material-ui-icons/MusicNote'
+import { withStyles } from 'material-ui/styles'
+import Grid from 'material-ui/Grid';
 
-const Landing = () => <h2>Welcome to Jonah's First Reactathon!</h2>
+const songListExample = [{name:"blah",artist:"Rolling Stones"},{name:"bloo",artist:"Moody Bloos"}]
 
-export default Landing
+const songListStyle = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+})
+
+const landingGridStyle = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+  }
+})
+
+const SongItem = songInfo => (
+  <ListItem button>
+    <ListItemIcon><MusicNoteIcon /></ListItemIcon>
+    <ListItemText primary={songInfo.name} secondary={songInfo.artist} />
+  </ListItem>
+)
+
+const SongList = ({classes, songList: [...songs]}) => (
+  <div className={classes.root}>
+    <h2>List of Songs</h2>
+    <List className={classes.root}>
+      {songs.map(s => SongItem(s))}
+    </List>
+  </div>
+)
+
+SongList.propTypes = {
+  classes: PropTypes.object.isRequired,
+  songList: PropTypes.array.isRequired
+}
+
+const StyledSongList = withStyles(songListStyle)(SongList)
+
+const Landing = ({classes}) => (
+  <div className={classes.root}>
+    <Grid container spacing={24}>
+      <Grid item xs={12}>
+        <StyledSongList songList={songListExample}></StyledSongList>
+      </Grid>
+    </Grid>
+  </div>
+)
+
+export default withStyles(landingGridStyle)(Landing)
