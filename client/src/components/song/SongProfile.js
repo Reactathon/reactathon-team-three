426e9b2f6c6604d272648bd554f3ca2ac8c6d352
violet-povor <violetvaliev@gmail.com>
2018-01-11 14:01:39 -0500

    Instrument selector in song profile

--- client/src/components/song/SongProfile.js
+++ client/src/components/song/SongProfile.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import TextField from 'material-ui/TextField';
 import { FormLabel, FormControl,FormGroup, FormControlLabel } from 'material-ui/Form';
 import Button from 'material-ui/Button';
+import InstrumentSelector from '../../components/InstrumentSelector/InstrumentSelector'
 import { addSong } from '../../actions/songActions'
 
 
@@ -26,13 +27,13 @@ class SongProfile extends Component {
             genre: this.state.genre
         };
         console.log("testing ", song);
-        
+
         this.props.addSong(song);
     }
 
     handleChange = name => event => {
         this.setState({ [name]: event.target.value });
-      };
+    };
 
     render() {
         return (
@@ -41,57 +42,54 @@ class SongProfile extends Component {
                 <form>
 
                     <FormControl component="fieldset">
-                    <FormGroup>
-                        <FormControlLabel
-                        control={
-                            <TextField
-                                id="name"
-                                label="Name"
-                                value={this.state.name}
-                                onChange={this.handleChange('name')}
-                                margin="normal"
-                            />
-                        }
-                        
-                    />
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControlLabel
-                        control={
-                            <TextField
-                                id="artist"
-                                label="Artist"
-                                value={this.state.artist}
-                                onChange={this.handleChange('artist')}
-                                margin="normal"
-                            />
-                        }
-                        
-                    />
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControlLabel
-                        control={
-                            <TextField
-                                id="genre"
-                                label="Genre"
-                                value={this.state.genre}
-                                onChange={this.handleChange('genre')}
-                                margin="normal"
-                            />
-                        }
-                        
-                    />
-                    </FormGroup>
-                </FormControl>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={
+                                    <TextField
+                                        id="name"
+                                        label="Name"
+                                        value={this.state.name}
+                                        onChange={this.handleChange('name')}
+                                        margin="normal"
+                                    />
+                                }
 
-                <div>
-                    <Button raised color="primary" onClick={this.handleSubmit}>Submit</Button>
-                </div>                
-                </form>  
-                
-                
-                
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={
+                                    <TextField
+                                        id="artist"
+                                        label="Artist"
+                                        value={this.state.artist}
+                                        onChange={this.handleChange('artist')}
+                                        margin="normal"
+                                    />
+                                }
+
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={
+                                    <TextField
+                                        id="genre"
+                                        label="Genre"
+                                        value={this.state.genre}
+                                        onChange={this.handleChange('genre')}
+                                        margin="normal"
+                                    />
+                                }
+
+                            />
+                        </FormGroup>
+                    </FormControl>
+                    <InstrumentSelector headerText={'Please select the instruments for this song'} selected={['Guitar','Violin']} toggleCallback={ value => { console.log(value)}} />
+                    <div>
+                        <Button raised color="primary" onClick={this.handleSubmit}>Submit</Button>
+                    </div>
+                </form>
             </div>
 
         );
